fba52c4af269b3c80d53cb08b17af6851df7bbf6
zerwin <1764912028@qq.com>
2019-02-02 19:29:44 +0800

    aaa

--- config/index.js
+++ config/index.js
@@ -10,7 +10,13 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+		'/product':{
+			target:'http://m.i360mall.com',
+			changeOrigin:true
+			
+		}
+	},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/test'
+import Home from '../components/home'
+import Tab from '../components/tab'
+import List from '../components/list'
+import Detail from '../components/detail'
+import Login from '../components/login'
+import Register from '../components/register'
 
 Vue.use(Router)
 
 export default new Router({
+	mode:'hash',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+		{
+		  path: 'tab/:kind',
+			component: Tab
+		},
+		{
+			path: '/list',
+			component: List
+		},
+		{
+			path: '/detail/:id',
+			component: Detail
+		},
+		{
+			path: '/login',
+			component: Login
+		},
+		{
+			path: '/ register',
+			component: Register
+		}
   ]
 })
